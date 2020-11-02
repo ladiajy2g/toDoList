62cbb84dfc9423d954ce6fbdb691c6ed20902fa3
ladiajy2g <ladi.ajayi@gmail.com>
2020-11-02 20:13:11 +0100

    Created all the Views

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname +'/date.js');
 
 const app = express();
 
+const items = [];
+const workItems = [];
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day ='';
-
-    switch (currentDay) {
-        case 0:
-            day ='Sunday';
-            break;
-        case 1:
-            day = 'Monday';
-            break;
-        case 2:
-            day = 'Tuesday';
-            break;
-        case 3:
-            day = 'Wednesday';
-            break;
-        
-    }
-   
-    res.render('list', {dayOfWeek: day});
+    const day = date.getDay();
+    res.render('list', {listTitle: day, newListItems: items});
 });
+
+
+app.post('/', (req, res) => {
+    const item = req.body.newItem;
+
+    if (req.body.list ==='Work'){
+        workItems.push(item);
+        res.redirect('/work');
+    }else {
+        items.push(item);
+        res.redirect('/');
+    }
+})
+
+app.get('/work', (req, res)=>{
+    res.render('list', {listTitle: 'Work List', newListItems: workItems});
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about');
+})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000.');
